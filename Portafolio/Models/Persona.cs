6a5ba07be28ce0ed308edabbb536a0507a278efb
francisco <fpa199725@gmail.com>
2022-10-12 10:43:03 -0500

    Incial

---
+++ Portafolio/Models/Persona.cs
@@ -0,0 +1,8 @@
+﻿namespace Portafolio.Models
+{
+    public class Persona
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,53 @@
+﻿
+using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo="Amazon",
+                    Descripcion="E-Commerce realizado en ASP.NET core",
+                    Link="https://amazon.com",
+                    ImagenURL="/imagenes/amazon.png"
+
+                },
+                   new Proyecto
+                {
+                    Titulo="Steam",
+                    Descripcion="Tienda en linea para comprar videojuegos",
+                    Link="https://store.steampowered.com",
+                    ImagenURL="/imagenes/steam.png"
+
+                },
+                   new Proyecto
+                {
+                    Titulo="New York Time",
+                    Descripcion=" Paginas de noticias",
+                    Link="https://nytime.com",
+                    ImagenURL="/imagenes/nyt.png"
+
+                },
+                   new Proyecto
+                {
+                    Titulo="Reddit",
+                    Descripcion="red social para compartir en comunidad",
+                    Link="https://reddit.com",
+                    ImagenURL="/imagenes/reddit.png"
+
+                },
+            };
+        }
+        }
+}
